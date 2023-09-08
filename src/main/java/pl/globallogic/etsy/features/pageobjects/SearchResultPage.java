ee016a9cf9713d9e2218e751fb32656d0e2bd1de
Jakub Bujak <kuba.bujak0@wp.pl>
2023-09-08 10:25:19 +0200

    "Created test for using free shipping filter"

--- src/main/java/pl/globallogic/etsy/features/pageobjects/SearchResultPage.java
+++ src/main/java/pl/globallogic/etsy/features/pageobjects/SearchResultPage.java
@@ -11,15 +11,21 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
 public class SearchResultPage {
     protected Logger logger = LoggerFactory.getLogger(SearchResultPage.class);
     private final WebDriver driver;
 
     private static final String OPEN_FILTER_BUTTON = "//button[@id='search-filter-button']";
-    private static final String FILTER_MIN_PRICE = "search-filter-min-price-input";
-    private static final String FILTER_MAX_PRICE = "search-filter-max-price-input";
+    private static final String INPUT_MIN_PRICE = "search-filter-min-price-input";
+    private static final String INPUT_MAX_PRICE = "search-filter-max-price-input";
     private static final String SEARCHED_ITEMS = "//ol[@data-results-grid-container]";
+    private static final String FREE_SHIPPING_CHECKBOX = "//label[@for='special-offers-free-shipping']";
+    private static final String APPLY_FILTER_BUTTON = "//button[@aria-label='Zastosuj']";
+    private static final String FILTERED_PRODUCTS_LIST = "//div[@data-search-results]//ol[@data-results-grid-container]";
+    private static final int MIN_PRICE_VALUE = generateRandomPrice(10, 50);
+    private static final int MAX_PRICE_VALUE = generateRandomPrice(MIN_PRICE_VALUE + 50, 150);
 
     public SearchResultPage(WebDriver driver) {
         this.driver = driver;
@@ -33,31 +39,73 @@ public class SearchResultPage {
         logger.info("Filter menu opened correctly");
     }
 
-    public void setMinAndMaxPriceToFilter(double minPrice, double maxPrice) {
-        WebElement minPriceInput = driver.findElement(By.id(FILTER_MIN_PRICE));
-        minPriceInput.sendKeys(minPrice + "");
-        WebElement maxPriceInput = driver.findElement(By.id(FILTER_MAX_PRICE));
-        maxPriceInput.sendKeys(maxPrice + "" + Keys.ENTER);
+    public void setMinAndMaxPriceToFilter() {
+        WebElement minPriceInput = driver.findElement(By.id(INPUT_MIN_PRICE));
+        minPriceInput.sendKeys(MIN_PRICE_VALUE + "");
+        WebElement maxPriceInput = driver.findElement(By.id(INPUT_MAX_PRICE));
+        maxPriceInput.sendKeys(MAX_PRICE_VALUE + "" + Keys.ENTER);
         WebElement usedFiltersButtonVisibility = new WebDriverWait(driver, Duration.ofSeconds(5)).
                 until(ExpectedConditions.visibilityOfElementLocated(By.xpath(SEARCHED_ITEMS)));
-        logger.info("Price filter set correctly");
+        logger.info("Price filter set correctly - searching between '{}' and '{}',", MIN_PRICE_VALUE, MAX_PRICE_VALUE);
     }
 
-    public boolean areItemsPriceBetweenMinAndMax(double minPrice, double maxPrice) {
-        WebElement filteredList = driver.findElement(By.xpath("//div[@data-search-results]//ol[@data-results-grid-container]"));
+    public void selectFreeShipping() {
+        WebElement freeShippingCheckbox = driver.findElement(By.xpath(FREE_SHIPPING_CHECKBOX));
+        freeShippingCheckbox.click();
+        logger.info("Free shipping correctly checked", MIN_PRICE_VALUE, MAX_PRICE_VALUE);
+        WebElement applyFilterButton = driver.findElement(By.xpath(APPLY_FILTER_BUTTON));
+        applyFilterButton.click();
+        logger.info("Apllied filters", MIN_PRICE_VALUE, MAX_PRICE_VALUE);
+    }
+
+    public static int generateRandomPrice(int minPrice, int maxPrice) {
+        if (minPrice >= maxPrice) {
+        throw new IllegalArgumentException("Min price must be lower than max price");
+        }
+        Random random = new Random();
+        return (int) (random.nextDouble(maxPrice - minPrice + 1) + minPrice);
+    }
+
+    public List<WebElement> getSearchedProductList() {
+        WebElement filteredList = new WebDriverWait(driver, Duration.ofSeconds(5)).until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath(FILTERED_PRODUCTS_LIST))
+        );
         List<WebElement> filteredItems = filteredList.findElements(By.tagName("li"));
         if (filteredItems.size() > 10) {
             filteredItems = filteredItems.subList(0, 10);
         }
+        return filteredItems;
+    }
+
+    public boolean areItemsPriceBetweenMinAndMax() {
+        List<WebElement> filteredItems = getSearchedProductList();
         for (WebElement item : filteredItems) {
             String currencyAmount = item.findElement(By.className("currency-value")).getText().replace(",", ".");
             double parsedAmount = Double.parseDouble(currencyAmount);
-            if (parsedAmount > maxPrice || parsedAmount < minPrice) {
-                logger.warn("Item price out of price range");
+            if (parsedAmount > MAX_PRICE_VALUE || parsedAmount < MIN_PRICE_VALUE) {
+                logger.info("Item number {} is out of range - item price is equal to '{}'", filteredItems.indexOf(item) + 1, currencyAmount);
                 return false;
             }
+            logger.info("Item number {} correct - item price is equal to '{}'", filteredItems.indexOf(item) + 1, currencyAmount);
         }
         return true;
+    }
+
+    public boolean areItemsFreeShippingFilteredCorrectly() {
+        boolean hasFreeShipping = false;
+        List<WebElement> filteredItems = getSearchedProductList();
+        for (WebElement item : filteredItems) {
+            String promotionBadgeLine = item.findElement(By.className("promotion-badge-line")).getText();
+            if (promotionBadgeLine.contains("Bezpłatna wysyłka")) {
+                logger.info("Item number {} has free shipping", filteredItems.indexOf(item) + 1);
+                hasFreeShipping = true;
+            } else {
+                logger.info("Item number {} has no free shipping - shipping is {}", filteredItems.indexOf(item) + 1, promotionBadgeLine);
+                hasFreeShipping = false;
+                return hasFreeShipping;
+            }
+        }
+        return hasFreeShipping;
     }
 
 
--- src/test/java/pl/globallogic/etsy/features/SearchResultFilteringTest.java
+++ src/test/java/pl/globallogic/etsy/features/SearchResultFilteringTest.java
@@ -2,6 +2,7 @@ package pl.globallogic.etsy.features;
 
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pl.globallogic.BaseLandingPageTest;
 import pl.globallogic.etsy.features.pageobjects.SearchResultPage;
@@ -12,48 +13,33 @@ import java.util.Random;
 
 public class SearchResultFilteringTest extends BaseLandingPageTest {
 
-    //Filter search result by price -> price didn't is lower than price value for filtering
-    @Test
-    public void searchResultPriceFitPriceRangeAfterFiltering(){
+    @BeforeMethod
+    public void testSetUp() {
         String validQuery = "leather bag";
-        double minPrice = generateRandomPrice(10, 50);
-        double maxPrice = generateRandomPrice(minPrice, 100);
         landingPage.searchFor(validQuery);
         Assert.assertTrue(landingPage.isSearchResultValidFor(validQuery));
         searchResultPage = new SearchResultPage(driver);
         searchResultPage.openFilterMenu();
-        searchResultPage.setMinAndMaxPriceToFilter(minPrice, maxPrice);
-        Assert.assertTrue(searchResultPage.areItemsPriceBetweenMinAndMax(minPrice, maxPrice));
     }
 
-    public int generateRandomPrice(double minPrice, double maxPrice) {if (minPrice >= maxPrice) {
-        throw new IllegalArgumentException("Min price must be lower than max price");
+    //Filter search result by price -> price didn't is lower than price value for filtering
+    @Test
+    public void searchResultPriceFitPriceRangeAfterFiltering(){
+        searchResultPage.setMinAndMaxPriceToFilter();
+        Assert.assertTrue(searchResultPage.areItemsPriceBetweenMinAndMax());
     }
 
-        Random random = new Random();
-        return (int) (random.nextDouble(maxPrice - minPrice + 1) + minPrice);
-    }
 
     //Filter search result by free shipping -> free shipping tag need to be present on all items
     @Test
     public void freeShippingTagNeedToBePresentAfterFiltering() {
-        String validQuery = "leather bag";
-        landingPage.searchFor(validQuery);
-        Assert.assertTrue(landingPage.isSearchResultValidFor(validQuery));
-        searchResultPage = new SearchResultPage(driver);
-        searchResultPage.openFilterMenu();
-        //select required filters ( by free shipping )
-        //apply selected filters
-        //verify free shipping tag/label is present on search result page
+        searchResultPage.selectFreeShipping();
+        Assert.assertTrue(searchResultPage.areItemsFreeShippingFilteredCorrectly());
     }
 
     @Test
     public void searchResultPageContentNotChangedIfFilterApplicationCanceled() {
-        String validQuery = "leather bag";
-        landingPage.searchFor(validQuery);
-        Assert.assertTrue(landingPage.isSearchResultValidFor(validQuery));
-        searchResultPage = new SearchResultPage(driver);
-        searchResultPage.openFilterMenu();
+
         //get 1st item in results title to be used in verification (after cancelling remain in-place)
         //expand filtering panel
         //cancel filter application
@@ -61,11 +47,7 @@ public class SearchResultFilteringTest extends BaseLandingPageTest {
     }
     @Test
     public void filteredColorSelectionOptionShouldBeAvailableInItemDetailsView() {
-        String validQuery = "leather bag";
-        landingPage.searchFor(validQuery);
-        Assert.assertTrue(landingPage.isSearchResultValidFor(validQuery));
-        searchResultPage = new SearchResultPage(driver);
-        searchResultPage.openFilterMenu();
+
         //select required filters ( by color )
         //apply selected filters
         //wait for search result to be filtered
@@ -74,11 +56,7 @@ public class SearchResultFilteringTest extends BaseLandingPageTest {
     }
     @Test
     public void filteredShippingCountryShouldBePresentInShippingDestinationOptions() {
-        String validQuery = "leather bag";
-        landingPage.searchFor(validQuery);
-        Assert.assertTrue(landingPage.isSearchResultValidFor(validQuery));
-        searchResultPage = new SearchResultPage(driver);
-        searchResultPage.openFilterMenu();
+
         //select required filters ( by shipping country )
         //apply selected filters
         //wait for search result to be filtered
